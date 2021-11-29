057700c95f86503a271fc7ad466c5748cc568cc6
Zina <ZDubinaQ@gmail.com>
2021-11-29 16:34:24 +0200

    feat(shared:styles): add common vars for colors and fontSizes

---
+++ src/shared/styles/variables.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  primaryGreen: '#7EB09B',
+  secondaryGreen: '#B3D0C4',
+  primaryOrange: '#E38164',
+  secondaryOrange: '#EFB9A9',
+  grey: '#94AEB3',
+  violet: '#824670',
+  white: '#fff',
+  black: '#000',
+};
+
+export const fontSizes = {
+  h1: 28,
+  h2: 24,
+  h3: 22,
+  subtitle1: 20,
+  subtitle2: 18,
+  middle: 15,
+  small: 14,
+  tiny: 12,
+};
